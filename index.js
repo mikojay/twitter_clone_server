8ac25753096c7f118a54a9429ac62838b5a01a3f
Jascha Mikolajski <jaschi.miko@web.de>
2019-07-11 22:22:07 +0700

    basisc server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const express = require('express')
+
+const app = express()
+
+app.listen((4000), (err) => {
+	if (err) {
+		console.log('ERROR', err);
+	} else {
+		console.log('Ready for Port 4000, baby');
+	}
+})

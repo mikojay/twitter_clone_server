b5bb43e5cc1b6963a78b121aeec392d81981dfea
Jascha Mikolajski <jaschi.miko@web.de>
2019-07-11 23:01:07 +0700

    add model and controller to create a message

--- controllers/post_message.js
+++ controllers/post_message.js
@@ -1,3 +1,15 @@
+const db_message = require('../models/message')
+
+
 module.exports = (req, res) => {
-	res.send('Message created')
+	db_message.create({
+		author: 'Chang Chang',
+		body: 'Hello World',
+		date: '11. Jul 2019',
+		hashtag: '#coding'
+	}).then((data) => {
+		res.send(data)
+	}).catch((err) => {
+		res.send(err)
+	})
 }

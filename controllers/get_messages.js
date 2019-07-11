0e03dd2ab4ae6ef3a66dd67e2deec27f7ee4cccd
Jascha Mikolajski <jaschi.miko@web.de>
2019-07-12 01:26:34 +0700

    populate email into get message

--- controllers/get_messages.js
+++ controllers/get_messages.js
@@ -4,6 +4,9 @@ module.exports = (req, res) => {
 	db_message.find({}).sort('-date').populate({
 		path: 'channel',
 		select: 'name'
+	}).populate({
+		path: 'author',
+		select: 'name email'
 	}).then((data) => {
 		res.send(data)
 	}).catch((err) => {

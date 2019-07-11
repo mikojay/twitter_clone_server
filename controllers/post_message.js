7df255c7abb1fb0f64deb69946cd3deb4d6a4562
Jascha Mikolajski <jaschi.miko@web.de>
2019-07-11 23:31:30 +0700

    create message from client

--- controllers/post_message.js
+++ controllers/post_message.js
@@ -1,12 +1,7 @@
 const db_message = require('../models/message')
 
 module.exports = (req, res) => {
-	db_message.create({
-		author: 'Chang Chang',
-		body: 'Hello World',
-		date: '11. Jul 2019',
-		hashtag: '#coding'
-	}).then((data) => {
+	db_message.create(req.body).then((data) => {
 		res.send(data)
 	}).catch((err) => {
 		res.send(err)

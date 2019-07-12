8437a7a9258e6afd433d5568d011c28f7825ee35
Jascha Mikolajski <jaschi.miko@web.de>
2019-07-12 10:53:30 +0700

    respond signup with token

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,5 +1,6 @@
 const db_user = require('../models/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 module.exports = (req, res) => {
 
@@ -8,8 +9,12 @@ module.exports = (req, res) => {
 			res.status(300).send('Error:', err)
 		} else {
 			req.body.password = encrypted
-			db_user.create(req.body).then((data) => {
-				res.send(data)
+			db_user.create(req.body).then((user) => {
+				let token = jwt.sign(user.toObject(), process.env.SECRET)
+				res.status(200).json({
+					message: 'You are signed up',
+					token: token
+				})
 			}).catch((err) => {
 				res.send(err)
 			})
